0d709b65ea82aa38eb066067ade8dd0042c3654f
Rionaldi <193679388+orionnion@users.noreply.github.com>
2025-08-24 20:59:54 +0700

    Solve privacy challenge

---
+++ src/EthernautPrivacy.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Privacy {
+    /**
+     * @notice uint8, uint16 takes only 1 and 2 bytes storage slot.
+     */
+    bool public locked = true; // 0
+    uint256 public ID = block.timestamp; // 1
+    uint8 private flattening = 10; // 2
+    uint8 private denomination = 255; // 2
+    uint16 private awkwardness = uint16(block.timestamp); // 2
+    bytes32[3] private data; // 3
+
+    constructor(bytes32[3] memory _data) {
+        data = _data;
+    }
+
+    function unlock(bytes16 _key) public {
+        require(_key == bytes16(data[2]));
+        locked = false;
+    }
+
+    /*
+    A bunch of super advanced solidity algorithms...
+
+      ,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`
+      .,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,
+      *.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^         ,---/V\
+      `*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.    ~|__(o.o)
+      ^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'  UU  UU
+    */
+}
